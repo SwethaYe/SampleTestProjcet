e9a079034f85b55363f982cf073bec3c7a248ca2
SwethaYe <swetha_y@dell.com>
2021-09-01 12:51:26 +0530

    Add project files.

---
+++ SampleTestProjcet/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SampleTestProjcet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Learn C# Sample!");
+            Console.ReadKey();
+        }
+    }
+}
